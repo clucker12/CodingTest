241e30a09660448d74cb5a18890f81578596b4e1
seong ho <412jsh@naver.com>
2025-06-10 14:07:21 +0900

    PCCE기출문제

---
+++ PCCE/PCCE1.java
@@ -0,0 +1,10 @@
+package PCCE;
+
+public class PCCE1 {
+    public static void main(String[] args) {
+        String message ="Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
